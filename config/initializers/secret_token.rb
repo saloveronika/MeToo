0ce930d8c2e4cb3e7ef88e28068249234abb8c9d
Bird <myrailsproject@gmail.com>
2013-01-19 18:54:51 +0200

    Create new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeToo::Application.config.secret_token = '1e569bab0e1f4bf62acac01008dee28835bcc802f1dd1d22fce79f98a3ccfc6455176e0a59909613d5f59da392a0921bf899c56d9182755d29fd4981fea2b57b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MeToo::Application.config.session_store :cookie_store, key: '_MeToo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MeToo::Application.config.session_store :active_record_store
